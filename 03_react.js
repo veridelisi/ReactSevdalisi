a61303c79dc34eebe53138fadd3d46ab5e7f5181
veridelisi <72710290+veridelisi@users.noreply.github.com>
2021-12-09 16:49:36 +0300

    Create 03_react.js

---
+++ 03_react.js
@@ -0,0 +1,53 @@
+app.js
+
+const Hello = (props) => {
+  const name = props.name
+  const age = props.age
+
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = "Su";
+  const age = 3;
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Engin" age={40} />
+      <Hello name={name} age={age} />
+    </div>
+  );
+};
+export default App;
+
+
+
+index.html
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Lesson 01</title>
+  </head>
+  <body>
+    <div id="app"></div>
+    <script src="index.js"></script>
+  </body>
+</html>
+
+
+index.js
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+ReactDOM.render(<App />, document.getElementById("root"));
