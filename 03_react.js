f33830f871944c8539ab1e07395f8fc46a5429b5
veridelisi <72710290+veridelisi@users.noreply.github.com>
2021-12-10 14:57:44 +0300

    Update 03_react.js

--- 03_react.js
+++ 03_react.js
@@ -1,17 +1,18 @@
 app.js
 
-      const Hello = (props) => {
+     const Hello = (props) => {
  //const Hello = ({ name, age }) => {
      const name = props.name
      const age = props.age
  //const { name, age } = props
 
-  const bornYear = () => new Date().getFullYear() - age
+
 
   return (
     <div>
+        
       <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
+   
     </div>
   )
 }
