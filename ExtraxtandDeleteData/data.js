8b2de75ba2219f3e9e86dc7f472b0a1f9d5c5db0
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-06-03 17:11:57 +0300

    Create data.js

---
+++ ExtraxtandDeleteData/data.js
@@ -0,0 +1,34 @@
+const data=[
+  {
+    id: 1,
+    name: 'Sabri Oncu',
+    age: 29,
+  
+  },
+  {
+    id: 2,
+    name: 'Engin Yilmaz',
+    age: 32,
+  
+  },
+  {
+    id: 3,
+    name: 'Baki Demirel',
+    age: 36,
+    
+  },
+  {
+    id: 4,
+    name: 'Emre Yilmaz',
+    age: 34,
+  
+  },
+  {
+    id: 5,
+    name: 'Su Yilmaz',
+    age: 29,
+   
+  },
+];
+
+export default data;
