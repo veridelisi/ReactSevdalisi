8c158d56a083a0a1b8e383e2299c15b7729bc7a9
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-06-03 14:21:00 +0300

    Create NewPage.js

---
+++ showandhide/NewPage.js
@@ -0,0 +1,13 @@
+import classes from "./NewPage.module.css";
+
+const NewPage= () => {
+
+    return (
+        <>
+        <h2 className={classes.header}>I'm New Page </h2>
+       
+        </>
+
+    )
+}
+export default NewPage;
