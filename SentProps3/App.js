cd6fd10f6bc49d1781b6718a5e56e7066c45abe2
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-09-23 15:16:43 +0300

    Create App.js

---
+++ SentProps3/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Joke from "./Joke"
+
+export default function App() {
+  
+  return (
+<>
+
+<Joke  punch="b" />
+<Joke  setup="a2" punch="bh"/>
+<Joke  setup="a3" punch="bn"/>
+<Joke  setup="a4" punch="bm"/>
+<Joke  setup="a5" punch="bll"/>
+
+</>
+  )
+}
