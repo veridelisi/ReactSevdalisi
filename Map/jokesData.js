3d9a727223e5c48b5568861aca957ddb4aa27bf6
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-10-20 11:26:19 +0300

    Update jokesData.js

--- Map/jokesData.js
+++ Map/jokesData.js
@@ -2,14 +2,17 @@ export default [
   {
     setup: "Engin",
     punchline: "40",
+    picture: "twitter.png"
   },
 
   {
     setup: "Emre",
     punchline: "30",
+    picture: "twitter.png"
   },
   {
     setup: "Su",
     punchline: "4",
+    picture: "twitter.png"
   },
 ];
