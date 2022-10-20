55a5edf35c801561bc5de5e47da7cb19b03c58b7
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-10-20 11:37:38 +0300

    Create App5.js

---
+++ Map/App5.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Joke from "./Joke";
+import jokesData from "./jokesData";
+
+export default function App() {
+  console.log(jokesData);
+
+  return (
+    <>
+      {jokesData.map((index) => {
+        return <Joke {...index} />;
+      })}
+    </>
+  );
+}
