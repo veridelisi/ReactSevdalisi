d5c0198a1db737e6e0c2d82356a8349b3d13e2f2
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-11-06 22:31:50 +0300

    Update and rename App4.js to App51.js

--- Map/App51.js
+++ Map/App51.js
@@ -2,7 +2,7 @@ import React from "react";
 import Explorer from "./Explorer";
 import Data from "./Data";
 
-export default function App4() {
+export default function App5() {
 
   const Veriler = Data.map((index) => {
     return <Explorer index={index}/>;
