efdc751739de2656574ddfc5b693de10759df423
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-11-06 22:19:59 +0300

    Update App2.js

--- Map/App2.js
+++ Map/App2.js
@@ -2,7 +2,7 @@ import React from "react";
 import Explorer from "./Explorer";
 import Data from "./Data";
 
-export default function App() {
+export default function App2() {
 
   const Veriler = Data.map((index) => {
     return <Explorer ad={index.ad} yas={index.yas} />;
