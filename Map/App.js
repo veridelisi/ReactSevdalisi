0df75be2f8fe853527e39de10e6e9b2fa72af8c3
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-11-06 21:35:52 +0300

    Update App.js

--- Map/App.js
+++ Map/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Joke from "./Joke";
 import jokesData from "./jokesData";
+import Joke from "./Joke";
 
 export default function App() {
-console.log(jokesData)
+
   const jokeElements = jokesData.map((joke) => {
     return <Joke setup={joke.setup} punchline={joke.punchline} />;
   });
