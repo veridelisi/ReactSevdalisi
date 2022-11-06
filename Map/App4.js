153d31048a9086cebbffb18fbeae744082971413
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-11-06 22:27:31 +0300

    Update App4.js

--- Map/App4.js
+++ Map/App4.js
@@ -1,15 +1,13 @@
 import React from "react";
-import Joke from "./Joke";
-import jokesData from "./jokesData";
+import Explorer from "./Explorer";
+import Data from "./Data";
 
-export default function App() {
-  console.log(jokesData);
+export default function App4() {
 
-  return (
-    <>
-      {jokesData.map((index) => {
-        return <Joke index={index} />;
-      })}
-    </>
-  );
+  const Veriler = Data.map((index) => {
+    return <Explorer index={index}/>;
+  });
+
+
+  return <>{Veriler}</>;
 }
