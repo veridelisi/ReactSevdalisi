da488a517b8d44fd4f04f649468e916b5746dbd3
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-11-06 22:21:34 +0300

    Update and rename Joke3.js to Explorer3.js

---
+++ Map/Explorer3.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Explorer({index}) {
+  return (
+    <>
+      <h3>{index.ad}</h3>
+       <h5>{index.yas}</h5>
+      <hr />
+    </>
+  );
+}
